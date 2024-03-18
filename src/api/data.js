dd4198b07dc0880608c50949e899d12074329f9b
BAssal <baslm8496@gmail.com>
2024-03-18 06:34:52 +0200

    adding data

---
+++ src/api/data.js
@@ -0,0 +1,11 @@
+ export const person = [
+    { id: "0", name: "حسن", date: "5 مساء", img: "p1.jpg" },
+    { id: "1", name: "محمد", date: "4 مساء", img: "p1.jpg" },
+    { id: "2", name: "على", date: "3 مساء", img: "p1.jpg" },
+    { id: "3", name: "محمود", date: "2 مساء", img: "p1.jpg" },
+    { id: "4", name: "حسن", date: "1 مساء", img: "p1.jpg" },
+    { id: "5", name: "حسن", date: "9 مساء", img: "p1.jpg" },
+    { id: "6", name: "تيتي", date: "5 مساء", img: "p1.jpg" },
+    { id: "7", name: "ابراهيم", date: "1.30 مساء", img: "p1.jpg" },
+    { id: "8", name: "حسام", date: "7 مساء", img: "p1.jpg" },
+  ];
